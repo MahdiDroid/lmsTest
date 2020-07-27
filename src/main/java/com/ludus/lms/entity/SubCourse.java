15f66623dd76a7f8977ec3806d0a682552ac7625
MahdiDroid <m.mhmvd.id@gmail.com>
2020-07-27 17:06:13 +0200

    sub course class  created

---
+++ src/main/java/com/ludus/lms/entity/SubCourse.java
@@ -0,0 +1,15 @@
+package com.ludus.lms.entity;
+
+import javax.persistence.Column;
+
+public class SubCourse {
+
+    private String name ;
+
+    private IntroVideo introVideo;
+    private LectureVideo lectureVideo;
+    private Reading  reading;
+    private InteractiveTool  interactiveTool;
+
+
+}
